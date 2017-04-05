bd25ac7279c051df93b4f1070f467c67640f6170
trangnth <nguyenhuyentrang1996@gmail.com>
2017-04-05 10:58:14 +0700

    1
    1

--- Script/nmap.scan.sh
+++ Script/nmap.scan.sh
@@ -7,12 +7,8 @@ VNDATE=`date "+%Y-%m-%d-%H%M"`
 SERVER="192.168.169.135"
 PORT="514"
 
-### File config rsyslog
 RSYSLOGCONF="/etc/rsyslog.d/nmap.conf"
-
 DATESCAN=`date '+%Y-%m-%d %H:%M:%S'`
-
-###File config nmap
 PATH_CONF="/home/huyentrang/nmap/"
 CONF_FILE="nmap.conf"
 
@@ -22,31 +18,35 @@ CONF_FILE="nmap.conf"
 CURRENT_VERSION=`nmap -V | grep version | cut -d ' ' -f3`
 
 if [ "x$CURRENT_VERSION" == "x" ]; then
-        echo "nmap is not installed, please check again"
-        exit 1
+	echo "nmap is not installed, please check again"
+	exit 1
 fi
 
 ### check config
+
 if [ ! -f $CONF_FILE ]; then
-        echo "No config file!"
-fi
+	echo "No config file!"
+fi 
 
 cd $PATH_CONF
 TARGET_SCAN=`cat $CONF_FILE | grep TARGET_SCAN | cut -f2`
 OPTIONS=`cat $CONF_FILE | grep OPTIONS | cut -f2`
 RUN=`cat $CONF_FILE | grep -w RUN | cut -f2`
-
+#echo $RUN
 if [[ $RUN == "n" ]]; then
-      exit 0
-fi
+	#echo "3"
+	exit 0
+fi 
 
 RESULTS=/var/log/nmap.results.log
 
 if [ -f $RESULTS ]; then
-        rm $RESULTS
-        touch $RESULTS
+	rm $RESULTS
+	touch $RESULTS
+	#echo "del"
 else
-        touch $RESULTS
+	touch $RESULTS
+#	chmod 660 $RESULTS
 fi
 
 # add config rsyslog
@@ -61,7 +61,7 @@ input(type="imfile"
 local3.* @$SERVER:$PORT
 EOL
 
-service rsyslog restart
+service rsyslog restart 
 
 # start scan
 echo "Defaut scan: DIRECTORY=$TARGET_SCAN ! "
